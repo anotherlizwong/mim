b348a91c6a35ed8c83de3b6126f4c331a05462c4
Liz Wong <anotherlizwong@gmail.com>
2017-07-18 22:47:14 -0400

    new mongodb stuff

--- app/mim/models.py
+++ app/mim/models.py
@@ -1,8 +1,21 @@
 import pymongo
+import os
 from pymongo import MongoClient
 from mongoengine import *
 
-client = MongoClient('localhost',27017)
+# client = MongoClient('localhost',27017)
+pw = os.environ("pw")
+database = os.environ("db")
+if pw:
+    client = pymongo.MongoClient("mongodb://obgynbbq-admin:" +
+                             pw +
+                             "@mimcluster-shard-00-00-mba0j.mongodb.net:27017," +
+                             "mimcluster-shard-00-01-mba0j.mongodb.net:27017," +
+                             "mimcluster-shard-00-02-mba0j.mongodb.net:27017/" +
+                             database +
+                             "?ssl=true&replicaSet=MimCluster-shard-0&authSource=admin")
+else:
+    client = MongoClient('localhost',27017)
 
 # get the sampleDB database
 
