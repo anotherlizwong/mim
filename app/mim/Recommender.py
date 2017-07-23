17d706522681c96ecf26bf1f02c5e78dc61d3ba8
Liz Wong <anotherlizwong@gmail.com>
2017-07-23 18:27:09 -0400

    start of recommender integration

---
+++ app/mim/Recommender.py
@@ -0,0 +1,12 @@
+import graphlab as gl
+
+
+def load_training_data():
+
+    training_data = gl.SFrame.read_json(url="test.json", orient="records")
+
+    pass
+
+def evaluate_option(option):
+    pass
+
